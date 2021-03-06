e23a5c86daa50e806592db94f9c33003c0469f5c
Dez Pringle <n305oul93@gmail.com>
2021-03-06 11:39:02 -0600

    completes task 1

---
+++ components/AboutComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Text, ScrollView } from 'react-native'
+
+class AboutComponent extends Component {
+  static navigationOptions = {
+    title: 'About Us'
+  }
+
+  render() {
+    return <ScrollView></ScrollView>
+  }
+}
+
+export default AboutComponent
