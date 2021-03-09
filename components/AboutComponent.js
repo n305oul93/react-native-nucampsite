0aab6eae170c417e7b4e313df9a3b9fc648b5886
Dez Pringle <n305oul93@gmail.com>
2021-03-09 11:43:00 -0600

    Configures redux
    sets up app to connect to redux store

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 import { Text, FlatList, ScrollView } from 'react-native'
 import { Card, ListItem } from 'react-native-elements'
-import { PARTNERS } from '../shared/partners'
+import { connect } from 'react-redux'
+import { baseUrl } from '../shared/baseUrl'
+
+const mapStateToProps = state => {
+  return {
+    partners: state.partners
+  }
+}
 
 function Mission() {
   return (
@@ -19,14 +26,7 @@ function Mission() {
   )
 }
 
-class AboutComponent extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      partners: PARTNERS
-    }
-  }
-
+class About extends Component {
   static navigationOptions = {
     title: 'About Us'
   }
@@ -37,19 +37,17 @@ class AboutComponent extends Component {
         <ListItem
           title={item.name}
           subtitle={item.description}
-          leftAvatar={{ source: require('./images/bootstrap-logo.png') }}
+          leftAvatar={{ source: { uri: baseUrl + item.image } }}
         />
       )
     }
-
-    console.log(this.state.partners)
 
     return (
       <ScrollView>
         <Mission />
         <Card title='Community Partners'>
           <FlatList
-            data={this.state.partners}
+            data={this.props.partners.partners}
             renderItem={renderPartner}
             keyExtractor={item => item.id.toString()}
           />
@@ -59,4 +57,4 @@ class AboutComponent extends Component {
   }
 }
 
-export default AboutComponent
+export default connect(mapStateToProps)(About)
